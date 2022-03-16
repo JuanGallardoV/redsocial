cef444f9b21b5d94a49b8b0547393143222bb8ab
Juan Gallardo <juangallardovergara17@gmail.com>
2022-03-16 20:59:25 -0300

    Implementado sistema de busqueda

--- redsocial_frontend/src/components/Feed.jsx
+++ redsocial_frontend/src/components/Feed.jsx
@@ -3,39 +3,44 @@ import { useParams } from 'react-router-dom';
 
 import { client } from '../client';
 import { feedQuery, searchQuery } from '../utils/data';
-import MasonryLayout from './MasonryLayout.jsx';
+import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
 const Feed = () => {
+  const [pins, setPins] = useState();
   const [loading, setLoading] = useState(false);
-  const [pins, setPins] = useState(null);
   const { categoryId } = useParams();
 
   useEffect(() => {
-    setLoading(true);
-    if(categoryId){
+    if (categoryId) {
+      setLoading(true);
       const query = searchQuery(categoryId);
-      
-      client.fetch(query)
-        .then((data) => {
-          setPins(data);
-          setLoading(false);
-        })
-    }else{
-      client.fetch(feedQuery)
-        .then((data) => {
-          setPins(data);
-          setLoading(false);
-        })
-    }
-  }, [categoryId])
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      setLoading(true);
 
-  if(loading) return <Spinner message="We are adding new ideas to your feed!"/>
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+  const ideaName = categoryId || 'new';
+  if (loading) {
+    return (
+      <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
+    );
+  }
   return (
     <div>
-        {pins && <MasonryLayout pins={pins} />}
+      {pins && (
+        <MasonryLayout pins={pins} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
